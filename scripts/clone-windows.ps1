6965d9a459bf6d416592a9b57a478d78fb696499
Swapan Lalotra <swapanlalotra@gmail.com>
2022-05-18 00:44:40 +0530

    Update clone-windows.ps1

--- scripts/clone-windows.ps1
+++ scripts/clone-windows.ps1
@@ -1,11 +1,11 @@
 cd ..
 
-git clone https://github.com/Swapan-lalotra/Theoneserver2.0/cloudflare.git
-git clone https://github.com/Swapan-lalotra/Theoneserver2.0/config.git
-git clone https://github.com/Swapan-lalotra/Theoneserver2.0/heroku.git
-git clone https://github.com/Swapan-lalotra/Theoneserver2.0/libdrive.github.io.git
-git clone https://github.com/Swapan-lalotra/Theoneserver2.0/server.git
-git clone https://github.com/Swapan-lalotra/Theoneserver2.0/web.git
-git clone https://github.com/Swapan-lalotra/Theoneserver2.0/webhooks.git
+git clone https://github.com/Swapan-lalotra/cloudflare.git
+git clone https://github.com/Swapan-lalotra/config.git
+git clone https://github.com/Swapan-lalotra/heroku.git
+git clone https://github.com/Swapan-lalotra/libdrive.github.io.git
+git clone https://github.com/Swapan-lalotra/server.git
+git clone https://github.com/Swapan-lalotra/web.git
+git clone https://github.com/Swapan-lalotra/webhooks.git
 
 cd libDrive
